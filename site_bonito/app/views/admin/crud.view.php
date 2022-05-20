02513fd743e26ca235dba8e35428847684e42213
Gustavo_Ferreira <gcarvalhaes10@gmail.com>
2022-05-19 21:04:57 -0300

    frontend arrumado

--- site_bonito/app/views/admin/crud.view.php
+++ site_bonito/app/views/admin/crud.view.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">    <title>Administrativo</title>
-    <link rel="stylesheet" href="../../public/css/admin.css">
-
-
+    
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/7a3ef7191d.js" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/7a3ef7191d.js" crossorigin="anonymous"></script>
-
+    
+    <link rel="stylesheet" href="../../public/css/admin.css">
 
 </head>
 <body>
