be2e7aa194d384bbcab258450aca2589a268e8ce
Gustavo_Ferreira <gcarvalhaes10@gmail.com>
2022-05-22 21:39:58 -0300

    ajeitando main para eles poderem clonar

--- site_bonito/app/routes.php
+++ site_bonito/app/routes.php
@@ -1,18 +1,8 @@
 <?php
 
-use App\Controllers\AdmController;
+use App\Controllers\Controller;
 use App\Core\Router;
 
-
-    $router->get('admin', 'AdmController@view');
-
-    $router->post('admin/create', 'AdmController@create');
-
-    $router->post('admin/delete', 'AdmController@delete');
-
-    $router->get('admin/show', 'AdmController@show');
-
-    $router->post('admin/update', 'AdmController@update');
 
 
 ?> 
